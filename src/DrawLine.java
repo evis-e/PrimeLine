54c1476e624a9f8fd140fbc201dcd0e891327117
evis <evis6174@gmail.com>
2016-02-05 00:53:09 +0200

    bug fix

--- src/DrawLine.java
+++ src/DrawLine.java
@@ -54,7 +54,7 @@ public class DrawLine extends JPanel {
         if(p == 2) return true;
         if(p < 3) return false;
         if(p % 2 == 0) return false;
-        for(int i = 3; i*i < p; i += 2) if (p % i == 0) return false;
+        for(int i = 3; i*i <= p; i += 2) if (p % i == 0) return false;
         return true;
     }
 
